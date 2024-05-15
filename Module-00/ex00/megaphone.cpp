63e5e59fe434a6587a5a9709893797c01bbeb899
Fred Rukundo <frukundo@e1r5p1.1337.ma>
2024-05-16 00:38:29 +0100

    use of strings

--- Module-00/ex00/megaphone.cpp
+++ Module-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:20:51 by frukundo          #+#    #+#             */
-/*   Updated: 2024/03/22 11:36:38 by frukundo         ###   ########.fr       */
+/*   Updated: 2024/05/16 00:33:26 by frukundo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 int main(int ac, char **av)
 {
    if (ac == 1)
-      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-   else
    {
-      for (int i = 1; av[i]; i++)
-      {
-         for (int j = 0; av[i][j]; j++)
-            std::cout << (char) toupper(av[i][j]);
-         if (i < ac -1)
-            std::cout << ' ';
-      }
+      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+      return (0);
+   }
+   for (int i = 1; i < ac; i++)
+   {
+      std::string str = av[i];
+      for (int j = 0; j < str.length(); j++)
+         std::cout << (char)toupper(str[j]);
+      if (i < ac - 1)
+         std::cout << ' ';
    }
    std::cout << std::endl;
    return (EXIT_SUCCESS);
