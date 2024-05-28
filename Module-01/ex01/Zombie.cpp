8590033a5617e531e21b193bc46f88426fa54b80
Fred Rukundo <frukundo@e1r3p1.1337.ma>
2024-05-28 03:05:05 +0100

    new changes

--- Module-01/ex01/Zombie.cpp
+++ Module-01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 04:55:04 by frukundo          #+#    #+#             */
-/*   Updated: 2024/05/17 05:14:02 by frukundo         ###   ########.fr       */
+/*   Updated: 2024/05/28 03:00:29 by frukundo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 Zombie::Zombie()
 {
-    this->name = "";
+}
+Zombie::Zombie(std::string name)
+{
+    this->name = name;
 }
 
 Zombie::~Zombie()
--- Module-01/ex01/Zombie.hpp
+++ Module-01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 04:39:42 by frukundo          #+#    #+#             */
-/*   Updated: 2024/05/17 05:00:39 by frukundo         ###   ########.fr       */
+/*   Updated: 2024/05/28 02:59:02 by frukundo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ class Zombie
    	std::string name;
 	public:
 		Zombie();
+		Zombie(std::string zombieName);
 		~Zombie();
 		void setName( std::string name);
 		void announce( void );
