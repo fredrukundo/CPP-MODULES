b541ca918320994f4236974eb5f68678ee016547
Fred Rukundo <frukundo@e1r3p1.1337.ma>
2024-05-29 01:43:37 +0100

    ex01

--- Module-01/ex01/zombieHorde.cpp
+++ Module-01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: frukundo <frukundo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 04:46:14 by frukundo          #+#    #+#             */
-/*   Updated: 2024/05/17 05:20:50 by frukundo         ###   ########.fr       */
+/*   Updated: 2024/05/29 01:40:55 by frukundo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 Zombie* zombieHorde( int N, std::string name)
 {
+     if (N <= 0) {
+        std::cerr << "Error: N must be a positive integer." << std::endl;
+        return nullptr;
+    }
     Zombie* z = new Zombie[N];
     for (int i = 0; i < N; i++)
         z[i].setName(name);
