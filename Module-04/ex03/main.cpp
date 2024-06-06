c7c45789b8a3d208d5d30ea30cb77ed2c975e3ec
Fred Rukundo <frukundo@e1r5p1.1337.ma>
2024-06-06 11:53:13 +0100

    cpp04

--- Module-04/ex03/main.cpp
+++ Module-04/ex03/main.cpp
@@ -3,27 +3,38 @@
 #include "Ice.hpp"
 #include "Character.hpp"
 
-int main()
+	int main()
 {
-	IMateriaSource *src = new MateriaSource();
+
+	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
 
-	ICharacter *me = new Character("me");
-	
-	AMateria *tmp;
-	tmp = src->createMateria("ice");
+	AMateria	*ice = src->createMateria("ice");
+	me->equip(ice);
+	me->unequip(0);
+	delete ice;
+
+	AMateria* tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	ICharacter *bob = new Character("bob");
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	me->unequip(3);
+	ICharacter* bob = new Character("bob");
 
 	me->use(0, *bob);
 	me->use(1, *bob);
-
+	me->use(2, *bob);
+	me->use(3, *bob);
 	delete bob;
 	delete me;
 	delete src;
+	
 
 	return 0;
 }
