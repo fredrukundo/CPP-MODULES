7a3d45b33eadc4337a2cb7235b87d6ed3f8afc44
Fred Rukundo <frukundo@e3r4p10.1337.ma>
2024-08-05 03:11:42 +0100

    cpp07

--- Module-07/ex01/iter.hpp
+++ Module-07/ex01/iter.hpp
@@ -15,8 +15,3 @@ void    iter( T *arr, size_t size, void (*f)(const T &) )
     for ( size_t i = 0; i < size; i++)
         f(arr[i]);
 }
-
-// template <typename T>
-// void print( T &i) {
-//     std::cout << i << std::endl;
-// }
